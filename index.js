ef8db3235377f7897fd747f713766df4e35ad491
Heorhii <gerythepatient@gmail.com>
2023-12-19 16:58:59 +0100

    delete needless imports

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -59,7 +56,5 @@ mongoose
 	})
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server port: ${PORT}`));
-		// User.insertMany(users);
-		// Post.insertMany(posts);
 	})
 	.catch(error => console.log(`${error} did not connect`));
