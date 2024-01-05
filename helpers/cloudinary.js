79683ccd211dfd2e54546ff252bd8c512cb94245
Heorhii <hmarkov.dev@gmail.com>
2024-01-05 17:49:36 +0100

    add cloudinary config

---
+++ helpers/cloudinary.js
@@ -0,0 +1,13 @@
+import cloud from "cloudinary";
+
+const cloudinary = cloud.v2;
+
+const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
+
+cloudinary.config({
+	cloud_name: CLOUDINARY_NAME,
+	api_key: CLOUDINARY_KEY,
+	api_secret: CLOUDINARY_SECRET,
+});
+
+export default cloudinary;
