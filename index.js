dd3ee3a7ec1915241eaff55774482eaddec0505e
Heorhii <hmarkov.dev@gmail.com>
2024-01-10 13:58:28 +0100

    dont ask

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
